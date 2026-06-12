02a12b26eaea0b0df34aca4f76734c067cb957d0
Marcin <dsw60365@student.ideis.pl>
2026-06-12 19:35:23 +0200

    Dopelniono obsluge krotkich flag oraz ukonczono help

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,7 @@ case "$1" in
     --date|-d)
         date
         ;;
+        
     --logs|-l)
         COUNT=${2:-100}
         for i in $(seq 1 $COUNT); do
@@ -11,18 +12,8 @@ case "$1" in
             echo "Data utworzenia: $(date)" >> "$FILENAME"
         done
         ;;
-    --help|-h)
-        echo "Dostępne opcje:"
-        echo "  --date, -d         Wyświetla dzisiejszą datę"
-        echo "  --logs, -l [N]     Tworzy N plików log (domyślnie 100)"
-        echo "  --help, -h         Wyświetla tę pomoc"
-        ;;
---init)
-        git clone https://github.com/Marcin60365/Narzedzia-IT-Lab4.git .
-        export PATH="$PATH:$(pwd)"
-        echo "Repozytorium sklonowane, ścieżka dodana do PATH."
-        ;;
---error|-e)
+        
+    --error|-e)
         COUNT=${2:-100}
         for i in $(seq 1 $COUNT); do
             mkdir -p "error${i}"
@@ -31,5 +22,25 @@ case "$1" in
             echo "Skrypt: $0" >> "$FILENAME"
             echo "Data utworzenia: $(date)" >> "$FILENAME"
         done
+        ;;
+        
+    --init)
+        git clone https://github.com/Marcin60365/Narzedzia-IT-Lab4.git .
+        export PATH="$PATH:$(pwd)"
+        echo "Repozytorium sklonowane, ścieżka dodana do PATH."
+        ;;
+        
+    --help|-h)
+        echo "Dostępne opcje:"
+        echo "  --date, -d         Wyświetla dzisiejszą datę"
+        echo "  --logs, -l [N]     Tworzy N plików log (domyślnie 100)"
+        echo "  --error, -e [N]    Tworzy N katalogów i plików error (domyślnie 100)"
+        echo "  --init             Klonuje repozytorium i ustawia PATH"
+        echo "  --help, -h         Wyświetla tę pomoc"
+        ;;
+        
+    *)
+        echo "Nieznana opcja. Użyj --help lub -h, aby zobaczyć dostępne flagi."
+        exit 1
         ;;
 esac
